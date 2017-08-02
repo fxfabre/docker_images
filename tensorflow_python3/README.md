de1d9ab50c2775cef31e9d58f22a3b2209586b5a
fxfabre <fx.fabre@gmail.com>
2017-08-02 23:43:57 +0100

    Build from sources

---
+++ tensorflow_python3/README.md
@@ -0,0 +1,10 @@
+Build Tensorflow from sources
+And create a docker image
+
+sources :
+- https://www.tensorflow.org/install/install_sources
+- https://github.com/tensorflow/tensorflow/tree/master/tensorflow/tools/docker
+
+Run build_from_sources.sh
+
+
---
+++ tensorflow_python3/build_from_sources.sh
@@ -0,0 +1,13 @@
+# see https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/docker
+
+export TF_DOCKER_BUILD_TYPE=CPU
+export TF_DOCKER_BUILD_IS_DEVEL=YES
+export TF_DOCKER_BUILD_DEVEL_BRANCH=r1.2
+export TF_DOCKER_BUILD_IMAGE_NAME=tensorflow_py3
+export TF_DOCKER_BUILD_VERSION=1.2
+export TF_DOCKER_BUILD_PYTHON_VERSION=PYTHON3
+
+./parameterized_docker_build.sh
+
+
+
